afe3cbd18aec3d44f85f13cc95d06a1181bb5614
trykov <me@trykov.ru>
2015-06-30 22:29:20 +0300

    Поправил bower и стартовое меню

--- www/js/homeCtrl.js
+++ www/js/homeCtrl.js
@@ -5,24 +5,8 @@
 app.controller('HomeCtrl', function ($scope, $ionicModal, $resource, $state, wsService, cardService) {
 
     $scope.play = function () {
-        $scope.modal.hide();
-        $scope.modal.remove();
         wsService.newConnection();
     };
-
-    $scope.showModal = function () {
-        $ionicModal.fromTemplateUrl('js/start/startMenu.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
-            $scope.modal.show()
-        })
-    };
-
-    $scope.$on('$destroy', function () {
-        $scope.modal.remove();
-    });
 
     /**
      * При открытии соединения переходим на доску
@@ -31,5 +15,4 @@ app.controller('HomeCtrl', function ($scope, $ionicModal, $resource, $state, wsS
         $state.go('desk');
     });
 
-    $scope.showModal();
 });
--- www/js/start/startMenu.js
+++
@@ -1,7 +0,0 @@
-/**
- * Mock for start menu ctrl, for now look home ctrl
- * @author trykov
- */
-app.controller('StartMenuCtrl', function($scope, $ionicModal) {
-
-});
